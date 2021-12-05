c6783a3f7f0e79fdce43596c081deb3a4fd85644
IAmrinderSingh <amrindersingh1703@gmail.com>
2021-12-05 14:00:46 +0530

    Rolling two dice on click

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,21 +10,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        val dice=Dice(6)
-        val diceRoll=dice.roll()
-        val resultTextView:TextView=findViewById(R.id.textView)
-        resultTextView.text=diceRoll.toString()
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val dice1 =Dice(6)
+        val diceRoll = dice.roll()
+        val diceRoll1=dice.roll()
+        // Update the screen with the dics roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        val resultTextView1: TextView =findViewById(R.id.textView1)
+        resultTextView.text = diceRoll.toString()
+        resultTextView1.text=diceRoll1.toString()
     }
 }
 
-class Dice(private val numSides: Int){
-    fun roll(): Int{
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
         return (1..numSides).random()
     }
 }
