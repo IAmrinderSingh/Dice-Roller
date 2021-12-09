792812b7d78df8ad7f9bb5b6d965622b30aeab25
IAmrinderSingh <amrindersingh1703@gmail.com>
2021-12-09 07:36:01 +0530

    changing image for diff random no

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -16,14 +16,18 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
-        val dice1 =Dice(6)
         val diceRoll = dice.roll()
-        val diceRoll1=dice.roll()
         // Update the screen with the dics roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        val resultTextView1: TextView =findViewById(R.id.textView1)
-        resultTextView.text = diceRoll.toString()
-        resultTextView1.text=diceRoll1.toString()
+        val diceImage: ImageView =findViewById(R.id.imageView)
+
+        when(diceRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 }
 
